69e82d0e4b18ad31e25c4564a713bfeee443fcef
Joseph Daniel Farrell <jdf4921@banjo.rit.edu>
2017-10-05 12:07:12 -0400

    Basic materialize integration

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 <html>
 <head>
 	<title>The Hub</title>
-	<link type="text/css" rel="stylesheet" href="assets/stylesheet.css"/>
-	<meta name"viewport" contains="width=device-width, initial-scale=1.0"/>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
 	<?php include 'header.html';?>
