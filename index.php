8c20b7a5824c68edc7b74a85fcf04a57b6264664
Joseph Daniel Farrell <jdf4921@banjo.rit.edu>
2017-10-05 11:33:30 -0400

    Switched to php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Hub</title>
+	<link type="text/css" rel="stylesheet" href="assets/stylesheet.css"/>
+	<meta name"viewport" contains="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+	<?php include 'header.php';?>
+	<h1>test</h1>
+</body>
+</html>
