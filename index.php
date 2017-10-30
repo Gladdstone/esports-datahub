0086169d8d93ffc49f6ee4c0beba77ae2016e7c7
Joseph Daniel Farrell <jdf4921@cs.rit.edu>
2017-10-30 11:18:57 -0400

    Fixed php header include

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-	<?php include 'header.html';?>
+	<?php include 'header.html' ?>
 	<h1>test</h1>
 </body>
 </html>
