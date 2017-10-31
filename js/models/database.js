72952e37a7af39e82fad63e949a50a5e271df4f2
Joseph Daniel Farrell <jdf4921@banjo.rit.edu>
2017-10-31 11:33:17 -0400

    Updated create query

--- js/models/database.js
+++ js/models/database.js
@@ -14,5 +14,5 @@ const connectionStr = process.env.DATABASE_URL || "postgres://localhost:5432/esp
 const client = new pg.Client(connectionStr);
 client.connect();
 const query = client.query(
-		"CREATE TABLE items(id SERIAL PRIMARY KEY, text CARCHAR(40) not null, complete boolean");	// Finalize before running
+		"CREATE TABLE player(username, first_name VARCHAR(40), last_name VARCHAR(40), team VARCHAR(40), game VARCHAR(40), PRIMARY KEY(username), FOREIGN KEY(team) references Teams(name), FOREIGN KEY(game) references Games(name))");	// Finalize before running
 query.on("end", () => { client.end(); });
